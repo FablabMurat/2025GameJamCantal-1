92bdd5a1eccd2e78d86e9ed48f5165de0e88975b
Thierry <flatweb@gmx.com>
2025-03-02 15:21:46 +0100

    missions des niveaux 1 à 5

--- root.gd
+++ root.gd
@@ -10,6 +10,8 @@ func _ready() -> void:
 	missions.append([0,1,1,0,0])
 	missions.append([0,2,1,1,0])
 	missions.append([0,3,2,1,0])
+	missions.append([0,3,2,1,1])
+	missions.append([0,2,2,2,1])
 	
 	runlevel()
 
