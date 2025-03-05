03b4bc16df1494480e28dde9a6a94e2aee0e8d36
Thierry <flatweb@gmx.com>
2025-03-05 09:41:03 +0100

    Préparation d'une fonction pour suspendre les effets d'une plante pendant 1,5 s  #21

--- plante.gd
+++ plante.gd
@@ -7,6 +7,8 @@ var canSpeedBoost : bool
 var canSwapPosition : bool
 var pickable : bool = false
 
+var effectsuspended : bool = false
+
 @export var stun_duration : float = 2
 @export var speed_boost_duration : float = 2
 @export_range(0, 1, 0.05) var speed_boost_strength : float = 0.5
@@ -53,12 +55,22 @@ func cueillir(body):
 		effetspecial(perso)
 
 func effetspecial(surperso):
+		if effectsuspended: return
 		if canStun:
 			surperso.apply_stun(stun_duration)
 		if canSpeedBoost:
 			surperso.apply_speed_boost(speed_boost_duration, speed_boost_strength)
 		if canSwapPosition:
 			swapPosition.emit()
+
+# Suspend les effets de la plante pendant 1.5 s
+func suspendeffect():
+	$SuspendTimer.start(1.5)
+	effectsuspended = true
+
+func _end_suspend_timer() -> void:
+	effectsuspended = false
+	pass # Replace with function body.
 
 func _on_body_exited(body: Node2D) -> void:
 	pickable = false
