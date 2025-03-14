bd404668454e501620ceb2bcba47ad2feacf5f87
Thierry <flatweb@gmx.com>
2025-03-14 19:21:26 +0100

    Commentaires sur les variables exportées de la classe Niveau

--- niveau.gd
+++ niveau.gd
@@ -1,12 +1,20 @@
 extends Node2D
 class_name Niveau
 
+## Contenu de la mission :
+## chaque élément du tableau est le nombre de fleurs à collecter par type.
+## Si la valeur est à 0, il pourra quand même y avoir des fleurs mises au hasard,
+## sinon aucune fleur du type n'apparaitra.
 @export var mission : Array[int] :
 	set(val) :
 		mission = val.duplicate()
 		allflowers.resize(mission.size())
-@export var moreflowers : int = 2
-@export var growableplants : bool = true
+## Nombre de fleurs apparaissant en plus de celles nécessaires pour la mission.
+@export var moreFlowers : int = 2
+## true si les plantes peuvent grandir et mourir.
+@export var growablePlants : bool = true
+## Durée maximale pour compléter le niveau.
+## A la fin, celui qui a collecté le plus de fleurs de la mission gagnera.
 @export var duree : int = 120
 
 var persos : Array[Perso]
@@ -61,7 +69,7 @@ func spawnplants():
 				addplant(i+1,newplant)
 
 	# plantes supplémentaires (décor et effets)
-	for i in range(moreflowers):
+	for i in range(moreFlowers):
 		var newplant = plantetscn.instantiate()
 
 		var rand = randf()
@@ -98,7 +106,7 @@ func addplant(idxplant, newplant):
 	newplant.position = fleurs_tilemap.map_to_local(random_cell);
 	newplant.position += Vector2(randi_range(-ecartmax,ecartmax),randi_range(-ecartmax,ecartmax))
 
-	newplant.settype(idxplant,growableplants)
+	newplant.settype(idxplant,growablePlants)
 	newplant.swapPosition.connect(swapplayers)
 	
 	add_child(newplant)
